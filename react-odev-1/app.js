20b5341979d80e5130294b1ea66e4259dd16bcec
RoDNes-root <beytullahrodnes@gmail.com>
2022-09-15 17:44:28 +0300

    react-odev-

---
+++ react-odev-1/app.js
@@ -0,0 +1,3 @@
+import getData from './index.js'; // index.js'i import ediyoruz
+
+console.log(await getData(1)); // import edilen index.js'deki getData fonksiyonunu kullanıyoruz ve 1'i parametre olarak veriyoruz
